80f8e0d62b06bcd97cb23876ec6ad5edd8109231
glitcher93 <nigeljdsouza@gmail.com>
2022-04-04 21:38:53 -0400

    created layout component to organize app component

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import HomeMain from "../HomeMain";
+import PageFooter from "../PageFooter";
+import PageHeader from "../PageHeader";
+
+const Layout = () => {
+    return ( 
+        <>
+            <PageHeader />
+            <HomeMain />
+            <PageFooter />
+        </>
+     );
+}
+ 
+export default Layout;
