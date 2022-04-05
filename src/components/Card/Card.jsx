b5d22e62f9fed549ac8573e2fac7a0ea8e9ab770
glitcher93 <nigeljdsouza@gmail.com>
2022-04-04 22:15:38 -0400

    added necessary components to corresponding routes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import InventoryList from '../../pages/InventoryList/InventoryList'
+import AddInventoryItem from '../../pages/AddInventoryItem/AddInventoryItem'
+import AddWarehouse from '../../pages/AddWarehouse'
+import EditInventoryItem from '../../pages/EditInventoryItem/EditInventoryItem'
+import EditWarehouse from '../../pages/EditWarehouse'
+import InventoryDetails from '../../pages/InventoryDetails/InventoryDetails'
+import InventoryList from '../../pages/InventoryList'
+import WarehouseDetails from '../../pages/WarehouseDetails'
 import WarehouseList from '../../pages/WarehouseList'
 import './Card.scss'
 
@@ -11,13 +17,13 @@ export default function Card() {
             <Routes>
                 <Route path='/' element={<WarehouseList />} />
                 <Route path='warehouses' element={<WarehouseList />} />
-                <Route path='warehouses/:id' />
-                <Route path='warehouses/add' />
-                <Route path='warehouses/edit/:id' />
+                <Route path='warehouses/:id' element={<WarehouseDetails />} />
+                <Route path='warehouses/add' element={<AddWarehouse />}/>
+                <Route path='warehouses/edit/:id' element={<EditWarehouse />} />
                 <Route path='inventory' element={<InventoryList />} />
-                <Route path='inventory/:id' />
-                <Route path='inventory/add' />
-                <Route path='inventory/edit/:id' />
+                <Route path='inventory/:id' element={<InventoryDetails />} />
+                <Route path='inventory/add' element={<AddInventoryItem />} />
+                <Route path='inventory/edit/:id' element={<EditInventoryItem />} />
             </Routes>
         </div>
     )
