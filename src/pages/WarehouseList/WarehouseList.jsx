48ef064527c6ced4c95fb735b8d44b6b7f02305f
glitcher93 <nigeljdsouza@gmail.com>
2022-04-04 21:43:22 -0400

    imported several utilities, added state, and finished off the rest this pages layout

--- src/pages/WarehouseList/WarehouseList.jsx
+++ src/pages/WarehouseList/WarehouseList.jsx
@@ -2,8 +2,24 @@ import Button from '../../components/Button';
 import './WarehouseList.scss';
 import Add from '../../assets/icons/add_white_24dp.svg';
 import TabletHeadings from '../../components/TabletHeadings';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import ItemList from '../../components/ItemList/ItemList';
+import { Link } from 'react-router-dom';
 
 export default function WarehouseList() {
+
+    const [warehouses, setWarehouses] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/warehouses')
+            .then((response) => {
+                setWarehouses(response.data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <section
         className='warehouse-list'
@@ -16,7 +32,9 @@ export default function WarehouseList() {
                 >
                     <h1
                     className='warehouse-list__title'
-                    >Warehouses</h1>
+                    >
+                        Warehouses
+                    </h1>
                 </div>
                 <div
                 className='warehouse-list__form-container'
@@ -30,10 +48,23 @@ export default function WarehouseList() {
                         className='warehouse-list__input'
                         />
                     </form>
-                    <Button text='Add New Warehouse' image={Add} />
+                    <Link 
+                    to='/warehouses/add'
+                    className='warehouse-list__link'
+                    >
+                        <Button 
+                        text='Add New Warehouse' 
+                        image={Add} 
+                        />
+                    </Link>
                 </div>
             </div>
-            <TabletHeadings headings={['Warehouse', 'Address', 'Contact Name', 'Contact Information']}/>
+            <TabletHeadings 
+            headings={['Warehouse', 'Address', 'Contact Name', 'Contact Information']}
+            />
+            <ItemList 
+            warehouses={warehouses}
+            />
         </section>
     )
 }
