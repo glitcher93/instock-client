94efd6733d6afe043eee1fc423811bb2262cbccf
glitcher93 <nigeljdsouza@gmail.com>
2022-04-04 21:44:01 -0400

    imported several utilities, added state, and finished off the rest of this pages layout

--- src/pages/InventoryList/InventoryList.jsx
+++ src/pages/InventoryList/InventoryList.jsx
@@ -2,8 +2,25 @@ import Button from '../../components/Button';
 import './InventoryList.scss';
 import Add from '../../assets/icons/add_white_24dp.svg';
 import TabletHeadings from '../../components/TabletHeadings';
+import ItemList from '../../components/ItemList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function InventoryList() {
+
+    const [inventory, setInventory] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:8080/inventory')
+            .then(response => {
+                setInventory(response.data)
+            })
+            .catch(err => console.log(err))
+
+    }, [])
+
     return (
         <section
         className='inventory-list'
@@ -32,10 +49,19 @@ export default function InventoryList() {
                         className='inventory-list__input'
                         />
                     </form>
-                    <Button text='Add New Item' image={Add} />
+                    <Link
+                    to='/inventory/add'
+                    className='inventory-list__link'
+                    >
+                        <Button 
+                        text='Add New Item' 
+                        image={Add} 
+                        />
+                    </Link>
                 </div>
             </div>
             <TabletHeadings headings={['Inventory Item', 'Category', 'Status', 'Qty', 'Warehouse']}/>
+            <ItemList inventory={inventory}/>
         </section>
     )
 }
