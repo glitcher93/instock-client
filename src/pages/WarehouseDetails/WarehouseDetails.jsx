8fbefbcff0c05ecb8fca2d613a940f990e48b842
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 17:07:59 -0400

    imported DeleteModal, added state for warehouseItemInfo, wrapped section tag in a main and two divs

--- src/pages/WarehouseDetails/WarehouseDetails.jsx
+++ src/pages/WarehouseDetails/WarehouseDetails.jsx
@@ -2,126 +2,174 @@ import './WarehouseDetails.scss';
 import arrow from "../../assets/icons/arrow_back-24px.svg";
 import edit from "../../assets/icons/edit_white-24px.svg";
 import { Link, useParams } from "react-router-dom";
-import { useState } from 'react';
-import warehouseData from '../../data/warehouses.json';
-import inventoryData from '../../data/inventories.json';
+import { useEffect, useState } from 'react';
 import Button from '../../components/Button';
 import TabletHeadings from '../../components/TabletHeadings';
 import ItemList from '../../components/ItemList';
+import { deleteItemFromWarehouse, getInventoryForWarehouse, getSingleWarehouse } from '../../utils/apiFunctions';
+import DeleteModal from '../../components/DeleteModal';
 
 const WarehouseDetails = () => {
 
-    const [warehouse, setWarehouse] = useState(warehouseData[0])
-    const foundInventory = inventoryData.filter(items => items.warehouseID === warehouse.id);
-    const [inventory, setInventory] = useState(foundInventory);
+    const { warehouseId } = useParams();
 
-    return (  
-        <section 
-        className="warehouse-details"
+    const [warehouse, setWarehouse] = useState({});
+    const [contact, setContact]  = useState({});
+    const [warehouseInventory, setWarehouseInventory] = useState([]);
+    const [show, setShow] = useState(false);
+    const [warehouseItemInfo, setWarehouseItemInfo] = useState({
+        id: "",
+        name: ""
+    })
+
+    useEffect(() => {
+        getSingleWarehouse(setWarehouse, setContact, warehouseId);
+        getInventoryForWarehouse(warehouseId, setWarehouseInventory)
+    }, [warehouseId])
+
+    const handleClose = () => {
+        setShow(false);
+    }
+
+    const handleDeleteItemFromWarehouse = (id) => {
+        deleteItemFromWarehouse(id, warehouseId, setWarehouseInventory)
+        setShow(false);
+    }
+
+    const {id, name, address, city, country } = warehouse;
+    const { name: contactName, position, phone, email } = contact
+
+    return (
+        <main 
+        className="main"
         >
-            <div
-            className="warehouse-details__header-container"
+            <div 
+            className="main__wrapper"
             >
-                <div className="warehouse-details__header">
-                    <Link
-                    to='/warehouses'
-                    >
-                        <img 
-                        src={arrow} 
-                        alt="Go Back" 
-                        />
-                    </Link>
-                    <h1
-                    className="warehouse-details__title"
-                    >
-                        {warehouse.name}
-                    </h1>
-                </div>
                 <div 
-                className="warehouse-details__button-container"
+                className="card"
                 >
-                    <Button
-                    image={edit}
-                    text="Edit"
-                    specialButton="button--special"
-                    specialClass="button__text--special"
-                    />
+                    <section 
+                    className="warehouse-details"
+                    >
+                        <div
+                        className="warehouse-details__header-container"
+                        >
+                            <div className="warehouse-details__header">
+                                <Link
+                                to='/warehouses'
+                                >
+                                    <img 
+                                    src={arrow} 
+                                    alt="Go Back" 
+                                    />
+                                </Link>
+                                <h1
+                                className="warehouse-details__title"
+                                >
+                                    {name}
+                                </h1>
+                            </div>
+                            <div 
+                            className="warehouse-details__button-container"
+                            >
+                                <Link
+                                to={`/warehouses/edit/${id}`}
+                                >
+                                    <Button
+                                    image={edit}
+                                    text="Edit"
+                                    specialButton="button--special"
+                                    specialClass="button__text--special"
+                                    />
+                                </Link>
+                            </div>
+                        </div>
+                        <div
+                        className='warehouse-details__container'
+                        >
+                            <div
+                            className='warehouse-details__details-container'
+                            >
+                                <article
+                                className='warehouse-details__details'
+                                >
+                                    <h2
+                                    className='warehouse-details__subtitle'
+                                    >
+                                        Warehouse Address:
+                                    </h2>
+                                    <p
+                                    className='warehouse-details__info warehouse-details__info--address'
+                                    >
+                                        {address}, {city}, {country}
+                                    </p>
+                                </article>
+                            </div>
+                            <div
+                            className='warehouse-details__details-container'
+                            >
+                                <article
+                                className='warehouse-details__details'
+                                >
+                                    <h2
+                                    className='warehouse-details__subtitle'
+                                    >
+                                        Contact Name:
+                                    </h2>
+                                    <p
+                                    className="warehouse-details__info"
+                                    >
+                                        {contactName}
+                                    </p>
+                                    <p
+                                    className="warehouse-details__info"
+                                    >
+                                        {position}
+                                    </p>
+                                </article>
+                                <article
+                                className='warehouse-details__details'
+                                >
+                                    <h2
+                                    className='warehouse-details__subtitle'
+                                    >
+                                        Contact Information:
+                                    </h2>
+                                    <p
+                                    className='warehouse-details__info'
+                                    >
+                                        {phone}
+                                    </p>
+                                    <p
+                                    className='warehouse-details__info'
+                                    >
+                                        {email}
+                                    </p>
+                                </article>
+                            </div>
+                        </div>
+                        <TabletHeadings 
+                        headings={["Inventory Item", "Category", "Status", "Quantity"]}
+                        specialClasses={["tablet-headings__container--details-item", "tablet-headings__container--details-category", "tablet-headings__container--details-status","tablet-headings__container--details-qty", "tablet-headings__container--details-actions"]}
+                        />
+                        <ItemList 
+                        warehouseInventory={warehouseInventory}
+                        setShow={setShow}
+                        setWarehouseItemInfo={setWarehouseItemInfo}
+                        detail
+                        />
+                    </section>
                 </div>
+                <DeleteModal 
+                show={show}
+                handleClose={handleClose}
+                warehouseItemInfo={warehouseItemInfo}
+                handleDeleteItemFromWarehouse={handleDeleteItemFromWarehouse} 
+                />
             </div>
-            <div
-            className='warehouse-details__container'
-            >
-                <div
-                className='warehouse-details__details-container'
-                >
-                    <article
-                    className='warehouse-details__details'
-                    >
-                        <h2
-                        className='warehouse-details__subtitle'
-                        >
-                            Warehouse Address:
-                        </h2>
-                        <p
-                        className='warehouse-details__info warehouse-details__info--address'
-                        >
-                            {warehouse.address}, {warehouse.city}, {warehouse.country}
-                        </p>
-                    </article>
-                </div>
-                <div
-                className='warehouse-details__details-container'
-                >
-                    <article
-                    className='warehouse-details__details'
-                    >
-                        <h2
-                        className='warehouse-details__subtitle'
-                        >
-                            Contact Name:
-                        </h2>
-                        <p
-                        className="warehouse-details__info"
-                        >
-                            {warehouse.contact.name}
-                        </p>
-                        <p
-                        className="warehouse-details__info"
-                        >
-                            {warehouse.contact.position}
-                        </p>
-                    </article>
-                    <article
-                    className='warehouse-details__details'
-                    >
-                        <h2
-                        className='warehouse-details__subtitle'
-                        >
-                            Contact Information:
-                        </h2>
-                        <p
-                        className='warehouse-details__info'
-                        >
-                            {warehouse.contact.phone}
-                        </p>
-                        <p
-                        className='warehouse-details__info'
-                        >
-                            {warehouse.contact.email}
-                        </p>
-                    </article>
-                </div>
-            </div>
-            <TabletHeadings 
-            headings={["Inventory Item", "Category", "Status", "Quantity"]}
-            specialClasses={["tablet-headings__container--details-item", "tablet-headings__container--details-category", "tablet-headings__container--details-status","tablet-headings__container--details-qty", "tablet-headings__container--details-actions"]}
-            />
-            <ItemList 
-            inventory={inventory}
-            detail
-            />
-        </section>
+        </main>  
     );
 }
- 
+
 export default WarehouseDetails;
