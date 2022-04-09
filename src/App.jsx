6f2842f41e1a923e8cb59129c300677e1f6ad58b
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 16:24:52 -0400

    migrated nested routes from Card to top level App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
-import Layout from './components/Layout/Layout';
+import PageHeader from './components/PageHeader';
+import PageFooter from './components/PageFooter';
+import WarehouseList from './pages/WarehouseList'
+import WarehouseDetails from './pages/WarehouseDetails';
+import AddWarehouse from './pages/AddWarehouse';
+import EditWarehouse from './pages/EditWarehouse';
+import InventoryList from './pages/InventoryList';
+import InventoryDetails from './pages/InventoryDetails';
+import AddInventoryItem from './pages/AddInventoryItem';
+import EditInventoryItem from './pages/EditInventoryItem';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <PageHeader />
         <Routes>
-          <Route path='/*' element={<Layout />}/>
+            <Route path="/*" element={<WarehouseList />} />
+            <Route path="warehouses" element={<WarehouseList />} />
+            <Route path="warehouses/:warehouseId" element={<WarehouseDetails />} />
+            <Route path="warehouses/add" element={<AddWarehouse />} />
+            <Route path="warehouses/edit/:warehouseId" element={<EditWarehouse />}/>
+            <Route path="inventory" element={<InventoryList />} />
+            <Route path="inventory/:itemId" element={<InventoryDetails />} />
+            <Route path="inventory/add" element={<AddInventoryItem />} />
+            <Route path="inventory/edit/:itemId" element={<EditInventoryItem />} />
         </Routes>
+        <PageFooter />
       </BrowserRouter>
     </>
   );
