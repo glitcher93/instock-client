bce6b4ba29d086e1789ee7c7b9da6a4cc3efb3d8
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 17:13:11 -0400

    set state for item, wrapped secion in a main and two divs

--- src/pages/EditInventoryItem/EditInventoryItem.jsx
+++ src/pages/EditInventoryItem/EditInventoryItem.jsx
@@ -1,33 +1,50 @@
 import './EditInventoryItem.scss';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import arrow from "../../assets/icons/arrow_back-24px.svg";
 import InventoryForm from '../../components/InventoryForm/InventoryForm';
+import { useEffect, useState } from 'react';
+import { getSingleItem } from '../../utils/apiFunctions';
 
 const EditInventoryItem = () => {
-    return ( 
-        <section
-        className="edit-item"
-        >
-            <div
-            className="edit-item__header-container"
-            >
-                <Link
-                to='/'
-                >
-                    <img 
-                    src={arrow} 
-                    alt="Go Back" 
-                    />
-                </Link>
-                <h1
-                className="edit-item__title"
-                >
-                    Edit Inventory Item
-                </h1>
+
+    const { itemId } = useParams();
+
+    const [item, setItem] = useState({})
+
+    useEffect(() => {
+        getSingleItem(itemId, setItem);
+    }, [itemId])
+
+    return (
+        <main className="main">
+            <div className="main__wrapper">
+                <div className="card">
+                    <section
+                    className="edit-item"
+                    >
+                        <div
+                        className="edit-item__header-container"
+                        >
+                            <Link
+                            to='/inventory'
+                            >
+                                <img 
+                                src={arrow} 
+                                alt="Go Back" 
+                                />
+                            </Link>
+                            <h1
+                            className="edit-item__title"
+                            >
+                                Edit Inventory Item
+                            </h1>
+                        </div>
+                        <InventoryForm item={item} />
+                    </section>
+                </div>
             </div>
-            <InventoryForm item />
-        </section>
+        </main>
     );
 }
- 
+
 export default EditInventoryItem;
