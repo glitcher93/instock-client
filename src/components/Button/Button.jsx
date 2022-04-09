aa5c3a5b4fdf2db79761612c691a9aa4ee3a49e7
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 16:33:22 -0400

    created conditional rendering function based on what prop is passed down to the button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,36 +1,105 @@
 import './Button.scss'
 
-export default function Button({ text, image, altText, specialButton, specialClass }) {
+export default function Button({ text, image, altText, specialButton, specialClass, handleClose, warehouseInfo, itemInfo, warehouseItemInfo, handleDeleteItem, handleDeleteWarehouse, handleDeleteItemFromWarehouse }) {
 
-    if (image) {
+    const renderButton = () => {
+        if (image) {
+            return (
+                <button
+                className={`button ${specialButton}`}
+                >
+                    <img 
+                    src={image} 
+                    alt={altText}
+                    className='button__image'
+                    />
+                    <p
+                    className={`button__text ${specialClass}`}
+                    >
+                        {text}
+                    </p>
+                </button>
+            )
+        }
+
+        if (handleDeleteWarehouse) {
+            return (
+                <button
+                className="button button--delete"
+                onClick={() => handleDeleteWarehouse(warehouseInfo.id)} 
+                >
+                    <p
+                    className="button__text"
+                    >
+                        {text}
+                    </p>
+                </button>
+            );
+            
+        }
+
+        if (handleDeleteItem) {
+            return (
+                <button
+                className="button button--delete"
+                onClick={() => handleDeleteItem(itemInfo.id)} 
+                >
+                    <p
+                    className="button__text"
+                    >
+                        {text}
+                    </p>
+                </button>
+            );
+        }
+
+        if (handleDeleteItemFromWarehouse) {
+            return (
+                <button
+                className="button button--delete"
+                onClick={() => handleDeleteItemFromWarehouse(warehouseItemInfo.id)} 
+                >
+                    <p
+                    className="button__text"
+                    >
+                        {text}
+                    </p>
+                </button>
+            );
+        }
+
+        if (text === "Cancel") {
+            return (
+                <button
+                className="button button--cancel"
+                onClick={handleClose} 
+                >
+                    <p
+                    className="button__text"
+                    >
+                        {text}
+                    </p>
+                </button>
+            )
+        }
+
         return (
             <button
-            className={`button ${specialButton}`}
+            className="button"
             >
-                <img 
-                src={image} 
-                alt={altText}
-                className='button__image'
-                />
                 <p
-                className={`button__text ${specialClass}`}
+                className="button__text"
                 >
                     {text}
                 </p>
             </button>
         )
-
     }
+    
 
     return (
-        <button
-        className={`button ${text === 'Cancel' ? "button--cancel" : ""}`}
-        >
-            <p
-            className={`button__text`}
-            >
-                {text}
-            </p>
-        </button>
+        <>
+            {renderButton()}
+        </>
     )
 }
