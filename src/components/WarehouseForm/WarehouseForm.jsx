ce939cda3874c44a95a70ded3e9a38e53d81b82c
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 17:35:39 -0400

    created state for form fields, created handlEdit, handleAdd, and handleOnChange functions

--- src/components/WarehouseForm/WarehouseForm.jsx
+++ src/components/WarehouseForm/WarehouseForm.jsx
@@ -1,186 +1,383 @@
 import "./WarehouseForm.scss";
 import Button from "../Button"
 import Add from '../../assets/icons/add_white_24dp.svg';
-import { Link } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { editWarehouse, postNewWarehouse } from "../../utils/apiFunctions";
+import { useEffect, useState } from "react";
+import ErrorMessage from "../ErrorMessage";
 
-const WarehouseForm = ({ warehouse }) => {
-  return (
-    <form 
-    className="warehouse-form"
-    >
-        <div className="warehouse-form__wrapper">
-            <div
-            className="warehouse-form__container"
-            >
-                <h2 
-                className="warehouse-form__sub-header"
+const WarehouseForm = ({ warehouse, contact }) => {
+
+    const { warehouseId } = useParams();
+    const navigate = useNavigate();
+    
+    const [warehouseName, setWarehouseName] = useState("");
+    const [warehouseAddress, setWarehouseAddress] = useState("");
+    const [warehouseCity, setWarehouseCity] = useState("");
+    const [warehouseCountry, setWarehouseCountry] = useState("");
+    const [contactPerson, setContactPerson] = useState("");
+    const [contactPosition, setContactPosition] = useState("");
+    const [contactPhone, setContactPhone] = useState("");
+    const [contactEmail, setContactEmail] = useState("");
+    const [nameRequired, setNameRequired] = useState(false);
+    const [addressRequired, setAddressRequired] = useState(false);
+    const [cityRequired, setCityRequired] = useState(false);
+    const [countryRequired, setCountryRequired] = useState(false);
+    const [contactNameRequired, setContactNameRequired] = useState(false);
+    const [positionRequired, setPositionRequired] = useState(false);
+    const [phoneRequired, setPhoneRequired] = useState(false);
+    const [emailRequired, setEmailRequired] = useState(false)
+
+    const handleOnChange = (e) => {
+        const { name: inputName, value } = e.target
+
+        switch (inputName) {
+            case "warehouseName":
+                setWarehouseName(value);
+                setNameRequired(false);
+                break;
+            case "address":
+                setWarehouseAddress(value);
+                setAddressRequired(false)
+                break;
+            case "city":
+                setWarehouseCity(value);
+                setCityRequired(false);
+                break;
+            case "country":
+                setWarehouseCountry(value);
+                setCountryRequired(false)
+                break;
+            case "contactName":
+                setContactPerson(value);
+                setContactNameRequired(false);
+                break;
+            case "position":
+                setContactPosition(value);
+                setPositionRequired(false);
+                break;
+            case "phone":
+                setContactPhone(value);
+                setPhoneRequired(false);
+                break;
+            case "email":
+                setContactEmail(value);
+                setEmailRequired(false)
+                break;
+            default: 
+                return;
+        }
+    }
+
+    const handleEdit = (e, id) => {
+        e.preventDefault();
+
+        const updatedWarehouse = {
+            name: warehouseName,
+            address: warehouseAddress,
+            city: warehouseCity,
+            country: warehouseCountry,
+            contact: {
+                name: contactPerson,
+                position: contactPosition,
+                phone: contactPhone,
+                email: contactEmail
+            }
+        };
+
+        editWarehouse(id, updatedWarehouse);
+
+        setTimeout(() => {
+            navigate('/')
+        }, 3000)
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault();
+
+        if (!warehouseName) {
+            setNameRequired(true);
+        }
+
+        if (!warehouseAddress) {
+            setAddressRequired(true);
+        }
+
+        if (!warehouseCity) {
+            setCityRequired(true);
+        }
+
+        if (!warehouseCountry) {
+            setCountryRequired(true);
+        }
+
+        if (!contactPerson) {
+            setContactNameRequired(true)
+        }
+
+        if (!contactPosition) {
+            setPositionRequired(true);
+        }
+
+        if (!contactPhone) {
+            setPhoneRequired(true);
+        }
+
+        if (!contactEmail) {
+            setEmailRequired(true)
+        }
+
+        if (!warehouseName || 
+            !warehouseAddress || 
+            !warehouseCity || 
+            !warehouseCountry || 
+            !contactPerson || 
+            !contactPosition || 
+            !contactPhone || 
+            !contactEmail ) {
+                alert("One or more fields are not filled out");
+                return;
+        }
+
+        const newWarehouse = {
+            name: warehouseName,
+            address: warehouseAddress,
+            city: warehouseCity,
+            country: warehouseCountry,
+            contact: {
+                name: contactPerson,
+                position: contactPosition,
+                phone: contactPhone,
+                email: contactEmail
+            }
+        };
+
+        postNewWarehouse(newWarehouse);
+
+        setTimeout(() => {
+            navigate('/')
+        }, 3000)
+    }
+
+    useEffect(() => {
+        if (warehouse) {
+            const { name, address, city, country} = warehouse;
+            const { name: contactName, position, phone, email } = contact;
+            setWarehouseName(name)
+            setWarehouseAddress(address)
+            setWarehouseCity(city)
+            setWarehouseCountry(country)
+            setContactPerson(contactName)
+            setContactPosition(position)
+            setContactPhone(phone)
+            setContactEmail(email)
+        }
+    }, [warehouse, contact])
+
+
+    return (
+        <form 
+        className="warehouse-form"
+        onSubmit={warehouse ? (e) => handleEdit(e, warehouseId) : (e) => handleAdd(e)}
+        >
+            <div className="warehouse-form__wrapper">
+                <div
+                className="warehouse-form__container"
                 >
-                    Warehouse Details
-                </h2>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="warehouseName" 
-                    className="warehouse-form__label"
+                    <h2 
+                    className="warehouse-form__sub-header"
                     >
-                        Warehouse Name
-                    </label>
-                    <input 
-                    type="text"
-                    id="warehouseName" 
-                    name="warehouseName"
-                    className="warehouse-form__input"
-                    placeholder={warehouse ? warehouse.name : "Warehouse Name"} 
-                    />
+                        Warehouse Details
+                    </h2>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="warehouseName" 
+                        className="warehouse-form__label"
+                        >
+                            Warehouse Name
+                        </label>
+                        <input 
+                        type="text"
+                        id="warehouseName" 
+                        name="warehouseName"
+                        value={warehouseName}
+                        onChange={handleOnChange}
+                        className={`warehouse-form__input ${nameRequired ? "warehouse-form__input--error" : ""}`}
+                        placeholder="Warehouse Name" 
+                        />
+                        {nameRequired && <ErrorMessage />}
                     </div>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="address" 
-                    className="warehouse-form__label"
+                    <div 
+                    className="warehouse-form__form-group"
                     >
-                        Street Address
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input"
-                    id="address"
-                    name="address"
-                    placeholder={warehouse ? warehouse.address : "Address"} 
-                    />
+                        <label 
+                        htmlFor="address" 
+                        className="warehouse-form__label"
+                        >
+                            Street Address
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${addressRequired ? "warehouse-form__input--error" : ""}`}
+                        id="address"
+                        name="address"
+                        value={warehouseAddress}
+                        onChange={handleOnChange}
+                        placeholder="Address" 
+                        />
+                        {addressRequired && <ErrorMessage />}
+                    </div>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="city" 
+                        className="warehouse-form__label"
+                        >
+                            City
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${cityRequired ? "warehouse-form__input--error" : ""}`}
+                        id="city"
+                        name="city"
+                        value={warehouseCity}
+                        onChange={handleOnChange}
+                        placeholder="City" 
+                        />
+                        {cityRequired && <ErrorMessage />}
+                    </div>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="country" 
+                        className="warehouse-form__label"
+                        >
+                            Country
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${countryRequired ? "warehouse-form__input--error" : ""}`} 
+                        id="country"
+                        name="country"
+                        value={warehouseCountry}
+                        onChange={handleOnChange}
+                        placeholder="Country"
+                        />
+                        {countryRequired && <ErrorMessage />}
+                    </div>
                 </div>
                 <div 
-                className="warehouse-form__form-group"
+                className="warehouse-form__container"
                 >
-                    <label 
-                    htmlFor="city" 
-                    className="warehouse-form__label"
+                    <h2 
+                    className="warehouse-form__sub-header"
                     >
-                        City
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input"
-                    id="city"
-                    name="city" 
-                    placeholder={warehouse ? warehouse.city : "City"} 
-                    />
-                    </div>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="country" 
-                    className="warehouse-form__label"
+                        Contact Details
+                    </h2>
+                    <div 
+                    className="warehouse-form__form-group"
                     >
-                        Country
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input" 
-                    id="country"
-                    name="country"
-                    placeholder={warehouse ? warehouse.country : "Country"}
-                    />
+                        <label 
+                        htmlFor="contactName" 
+                        className="warehouse-form__label"
+                        >
+                            Contact Name
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${contactNameRequired ? "warehouse-form__input--error" : ""}`}
+                        id="contactName"
+                        name="contactName"
+                        value={contactPerson}
+                        onChange={handleOnChange}
+                        placeholder="Contact Name"
+                        />
+                        {contactNameRequired && <ErrorMessage />}
                     </div>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="position" 
+                        className="warehouse-form__label">
+                            Position
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${positionRequired ? "warehouse-form__input--error" : ""}`}
+                        id="position"
+                        name="position"
+                        value={contactPosition}
+                        onChange={handleOnChange}
+                        placeholder="Position"
+                        />
+                        {positionRequired && <ErrorMessage />}
+                    </div>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="phone" 
+                        className="warehouse-form__label"
+                        >
+                            Phone Number
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${phoneRequired ? "warehouse-form__input--error" : ""}`}
+                        id="phone"
+                        name="phone"
+                        value={contactPhone}
+                        onChange={handleOnChange}
+                        placeholder="Phone Number"
+                        />
+                        {phoneRequired && <ErrorMessage />}
+                    </div>
+                    <div 
+                    className="warehouse-form__form-group"
+                    >
+                        <label 
+                        htmlFor="email" 
+                        className="warehouse-form__label"
+                        >
+                            Email
+                        </label>
+                        <input 
+                        type="text" 
+                        className={`warehouse-form__input ${emailRequired ? "warehouse-form__input--error" : ""}`}
+                        id="email"
+                        name="email"
+                        value={contactEmail}
+                        onChange={handleOnChange}
+                        placeholder="Email"
+                        />
+                        {emailRequired && <ErrorMessage />}
+                    </div>
+                </div>
             </div>
             <div 
-            className="warehouse-form__container"
+            className="warehouse-form__buttons"
             >
-                <h2 
-                className="warehouse-form__sub-header"
+                <Link
+                to='/'
+                className="warehouse-form__link"
                 >
-                    Contact Details
-                </h2>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="contactName" 
-                    className="warehouse-form__label"
-                    >
-                        Contact Name
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input"
-                    id="contactName"
-                    name="contactName"
-                    placeholder={warehouse ? warehouse.contact.name : "Contact Name"}
+                    <Button 
+                    text="Cancel"
                     />
-                </div>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="position" 
-                    className="warehouse-form__label">
-                        Position
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input"
-                    id="position"
-                    name="position"
-                    placeholder={warehouse ? warehouse.contact.position : "Position"}
-                    />
-                </div>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="phone" 
-                    className="warehouse-form__label"
-                    >
-                        Phone Number
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input" 
-                    id="phone"
-                    name="phone"
-                    placeholder={warehouse ? warehouse.contact.phone : "Phone Number"}
-                    />
-                </div>
-                <div 
-                className="warehouse-form__form-group"
-                >
-                    <label 
-                    htmlFor="email" 
-                    className="warehouse-form__label"
-                    >
-                        Email
-                    </label>
-                    <input 
-                    type="text" 
-                    className="warehouse-form__input"
-                    id="email"
-                    name="email" 
-                    placeholder={warehouse ? warehouse.contact.email : "Email"}
-                    />
-                </div>
-            </div>
-        </div>
-        <div 
-        className="warehouse-form__buttons"
-        >
-            <Link
-            to='/'
-            className="warehouse-form__link"
-            >
+                </Link>
                 <Button 
-                text="Cancel"
+                text={warehouse ? "Save" : "Add Warehouse"}
+                image={warehouse ? "" : Add}
+                altText="Add"
                 />
-            </Link>
-            <Button 
-            text={warehouse ? "Save" : "Add Warehouse"}
-            image={warehouse ? "" : Add}
-            />
-        </div>
-    </form>
-  );
+            </div>
+        </form>
+    );
 };
 
 export default WarehouseForm;
