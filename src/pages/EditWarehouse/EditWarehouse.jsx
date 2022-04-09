c98081f687c18a2492bc97ebe4ef0eae2621f760
glitcher93 <nigeljdsouza@gmail.com>
2022-04-09 17:11:39 -0400

    set state for warehouse and contact, wrapped section in a main and two divs

--- src/pages/EditWarehouse/EditWarehouse.jsx
+++ src/pages/EditWarehouse/EditWarehouse.jsx
@@ -1,32 +1,53 @@
 import './EditWarehouse.scss';
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import arrow from "../../assets/icons/arrow_back-24px.svg";
 import WarehouseForm from '../../components/WarehouseForm/WarehouseForm';
+import { useEffect, useState } from 'react';
+import { getSingleWarehouse } from '../../utils/apiFunctions';
 
 const EditWarehouse = () => {
-    return ( 
-        <section
-        className="edit-warehouse"
-        >
-            <div
-            className="edit-warehouse__header-container"
-            >
-                <Link
-                to='/'
-                >
-                    <img 
-                    src={arrow} 
-                    alt="Go Back" 
-                    />
-                </Link>
-                <h1
-                className="edit-warehouse__title"
-                >
-                    Edit Warehouse
-                </h1>
+
+    const { warehouseId } = useParams();
+
+    const [warehouse, setWarehouse] = useState({});
+    const [contact, setContact] = useState({});
+
+    useEffect(() => {
+        getSingleWarehouse(setWarehouse, setContact, warehouseId)
+    }, [warehouseId])
+
+    return (
+        <main className="main">
+            <div className="main__wrapper">
+                <div className="card">
+                    <section
+                    className="edit-warehouse"
+                    >
+                        <div
+                        className="edit-warehouse__header-container"
+                        >
+                            <Link
+                            to='/'
+                            >
+                                <img 
+                                src={arrow} 
+                                alt="Go Back" 
+                                />
+                            </Link>
+                            <h1
+                            className="edit-warehouse__title"
+                            >
+                                Edit Warehouse
+                            </h1>
+                        </div>
+                        <WarehouseForm 
+                        warehouse={warehouse} 
+                        contact={contact} 
+                        />
+                    </section>
+                </div>
             </div>
-            <WarehouseForm warehouse />
-        </section>
+        </main> 
     );
 }
 
